7b87429073fb992c3c96a92fc8ceeb0b7299d420
Gergő Peti <96701860+GergoPeti@users.noreply.github.com>
2023-04-12 21:27:24 +0200

    Generate random numbers, rendering the numbers

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
 'use strict';
 
-const heroWizard = {
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1;
+  });
+}
+
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join('');
+}
+
+const hero = {
   elementId: 'hero',
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceScore: 6,
+  diceCount: 3,
 };
 
-const monsterOrc = {
+const monster = {
   elementId: 'monster',
   name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceScore: 4,
+  diceCount: 1,
 };
 
-const renderCharacter = function (elementId, name, avatar, health, diceScore) {
-  return (document.getElementById(
-    `${elementId}`
-  ).innerHTML = `<div class="character-card">
-        <h4 class="name">${name}</h4>
-        <img class="avatar" src=${avatar}/>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${diceScore} </div></div>
-    </div>`);
-};
+function renderCharacter(data) {
+  const { elementId, name, avatar, health, diceCount } = data;
+  const diceHtml = getDiceHtml(diceCount);
 
-renderCharacter(
-  heroWizard.elementId,
-  heroWizard.name,
-  heroWizard.avatar,
-  heroWizard.health,
-  heroWizard.diceScore
-);
-renderCharacter(
-  monsterOrc.elementId,
-  monsterOrc.name,
-  monsterOrc.avatar,
-  monsterOrc.health,
-  monsterOrc.diceScore
-);
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">    
+                ${diceHtml}
+            </div>
+        </div>`;
+}
 
-// document.getElementById("hero").innerHTML =
-//     `<div class="character-card">
-//         <h4 class="name"> Wizard </h4>
-//         <img class="avatar" src="images/wizard.png"/>
-//         <p class="health">health: <b> 60 </b></p>
-//         <div class="dice-container"><div class="dice"> 6 </div></div>
-//     </div>`;
-
-// document.getElementById("monster").innerHTML =
-//     `<div class="character-card">
-//         <h4 class="name"> Orc </h4>
-//         <img class="avatar" src="images/orc.png"/>
-//         <p class="health">health: <b> 10 </b></p>
-//         <div class="dice-container"><div class="dice"> 4 </div></div>
-//     </div>`;
+renderCharacter(hero);
+renderCharacter(monster);
